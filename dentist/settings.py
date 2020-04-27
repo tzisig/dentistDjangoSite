983472440edcc2686e0e4bc2ea562cf1f1d48cda
Tzion Sigron <univreno@gmail.com>
2020-04-27 17:41:05 +0300

    Tweaked app for heroku

--- dentist/settings.py
+++ dentist/settings.py
@@ -1,4 +1,7 @@
 import os
+import django_heroku
+import dj_database_url
+from decouple mport config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -36,6 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'dentist.urls'
@@ -112,6 +116,8 @@ STATIC_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATIC_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # myaccount.google.com/lesssecureapps
 # account.google.com/DisplayUnlockCapcha
@@ -124,3 +130,6 @@ EMAIL_HOST_USER = 'eladreck@gmail.com'
 EMAIL_HOST_PASSWORD = 'tied119joy078'
 EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = False
+
+
+django_heroku.settings(locals())
